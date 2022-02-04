38299d19c424659f3b859433e00e334e992c1838
vineboneto <vineboneto@gmail.com>
2022-02-04 10:31:12 -0300

    feat: create home

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from './header'
+export * from './sign-in-button'
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
